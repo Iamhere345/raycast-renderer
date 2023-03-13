2f39c1dca8569b1da3a419219d576e7bb4ac666b
Iamhere345 <lunarome08@icloud.com>
2023-03-13 17:54:23 +1100

    add raycasting logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use winit::{event_loop::{EventLoop}, event::{Event, WindowEvent}, window::WindowBuilder, dpi::LogicalSize};
+use std::collections::HashMap;
+
 use winit::event_loop::EventLoop;
-use winit::event::{Event, WindowEvent};
+use winit::event::{Event, WindowEvent, ElementState};
 use winit::window::WindowBuilder;
 use winit::dpi::LogicalSize;
 use winit::event::VirtualKeyCode;
@@ -13,7 +14,7 @@ const MAP_HEIGHT: usize = 24;
 const SCREEN_WIDTH: usize = 640;
 const SCREEN_HEIGHT: usize = 480;
 
-const MAP: [[u32; 0]; 0] =
+const MAP: [[u32; MAP_WIDTH]; MAP_HEIGHT] =
 [
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -40,6 +41,8 @@ const MAP: [[u32; 0]; 0] =
   [1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
+
+const WALL_HEIGHT: i32 = 20;
 
 type PixelBuf = [u32; SCREEN_WIDTH * SCREEN_HEIGHT];
 
@@ -85,7 +88,7 @@ fn main() {
 
     let window = WindowBuilder::new()
         .with_title("Raycast Renderer")
-        .with_inner_size(LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT))
+        .with_inner_size(LogicalSize::new(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32))
         .with_resizable(false)
         .build(&event_loop)
         .expect("Unable to create window.");
@@ -118,12 +121,127 @@ fn render(framebuffer: &PixelBuf) {}
 
 fn update(screen: &mut PixelBuf, player: &mut Player) {
 
+    let WALL_COLOURS: HashMap<&str, (u8, u8, u8)> = HashMap::from([
+        ("Red", (255, 0, 0)),
+        ("Green", (0, 255, 0)),
+        ("Blue", (0, 0, 255)), 
+        ("White", (255, 255, 255)),
+        ("Teal", (159, 252, 253)),
+        ("Yellow", (255, 253, 85))
+    ]);
+
     for x in 0..SCREEN_WIDTH {
 
         let camera_x: f64 = 2.0 * x as f64 / SCREEN_WIDTH as f64;
-        let ray = Vec2::<f64>::new(player.dir.x + player.plane.x * camera_x, player.dir.y + player.plane.y * camera_x);
-        let ray_dir_x: f64 = player.dir.x + player.plane.x * camera_x;
+        let ray_dir = Vec2::<f64>::new(player.dir.x + player.plane.x * camera_x, player.dir.y + player.plane.y * camera_x);
+        //let ray_dir_x: f64 = player.dir.x + player.plane.x * camera_x;
+
+        // DDA setup
+        let mut map_pos = Vec2::<i32>::new(player.pos.x.floor() as i32, player.pos.y.floor() as i32);
+        let mut side_dist = Vec2::<f64>::new(0.0, 0.0);
+
+        let delta_dist = Vec2::<f64>::new(if ray_dir.x == 0.0 { f64::INFINITY } else { (1.0 / ray_dir.x).abs() }, if ray_dir.y == 0.0 { f64::INFINITY } else { (1.0 / ray_dir.y).abs() });
+        let mut perp_wall_dist: f64;
+
+        let mut step = Vec2::<i32>::new(0, 0);
+        
+        let mut hit: bool = false;
+        let mut side: u8 = 0;
+
+        // get initial step and side dist values
+        if ray_dir.x < 0.0 {
+            step.x = -1;
+            side_dist.x = (player.pos.x - map_pos.x as f64) * delta_dist.x;
+        } else {
+            step.x = 1;
+            side_dist.x = (map_pos.x as f64 + 1.0 - player.pos.x) * delta_dist.x;
+        }
+
+        if ray_dir.y < 0.0 {
+            step.y = -1;
+            side_dist.y = (player.pos.y - map_pos.y as f64) * delta_dist.y;
+        } else {
+            step.y = 1;
+            side_dist.y = (map_pos.y as f64 + 1.0 - player.pos.y) * delta_dist.y;
+        }
+
+        // DDA raycast
+        while !hit {
+
+            if side_dist.x < side_dist.y {
+                side_dist.x += delta_dist.x;
+                map_pos.x += step.x;
+                side = 0;
+            } else {
+                side_dist.y += delta_dist.y;
+                map_pos.y += step.y;
+                side = 1;
+            }
+
+            if MAP[map_pos.x as usize][map_pos.y as usize] > 0 {
+                hit = true;
+            }
+
+        }
+
+        if side == 0 {
+            perp_wall_dist = side_dist.x - delta_dist.x;
+        } else {
+            perp_wall_dist = side_dist.y - delta_dist.y;
+        }
+
+        let line_height: i32 = WALL_HEIGHT / perp_wall_dist as i32;
+
+        let mut draw_start: i32 = -line_height / 2 + WALL_HEIGHT / 2;
+        let mut draw_end: i32 = line_height / 2 + WALL_HEIGHT / 2;
+
+        if draw_start < 0 {
+            draw_start = 0
+        }
+
+        if draw_end > WALL_HEIGHT {
+            draw_end = WALL_HEIGHT - 1;
+        }
+
+        let wall_colour_unpacked = match MAP[map_pos.x as usize][map_pos.y as usize] {
+            1 => WALL_COLOURS.get(&"Red"),
+            2 => WALL_COLOURS.get(&"Green"),
+            3 => WALL_COLOURS.get(&"Blue"),
+            4 => WALL_COLOURS.get(&"Teal"),
+            _ => WALL_COLOURS.get(&"White")
+        }.unwrap();
+
+        let mut wall_colour = pack_colour(wall_colour_unpacked.0, wall_colour_unpacked.1, wall_colour_unpacked.2);
+
+        // give walls a different brightness
+        if side == 1 {
+            wall_colour /= 2;
+        }
+
+        draw_line(screen, x, draw_start as usize, draw_end as usize, wall_colour);
 
     }
 
+}
+
+fn draw_line(framebuffer: &mut PixelBuf, x: usize, y0: usize, y1: usize, colour: u32) {
+
+    for y in y0..y1 {
+        framebuffer[x + y * SCREEN_WIDTH] = colour;
+    }
+
+}
+
+// TODO
+fn pack_colour(r: u8, g: u8, b: u8) -> u32 {
+    ((0 as u32) << 24) + ((b as u32) << 16) + ((g as u32) << 8) + (r as u32)
+}
+
+fn unpack_color(color: &u32) -> (u8, u8, u8, u8) {
+    let r: u8 = (color & 255) as u8;
+    let g: u8 = ((color >> 8) & 255) as u8;
+    let b: u8 = ((color >> 16) & 255) as u8;
+    let a: u8 = ((color >> 24) & 255) as u8;
+
+    (r, g, b, a)
 }
